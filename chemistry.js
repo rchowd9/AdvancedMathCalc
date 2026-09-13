46233ddb4e1a59034a90cdfeca723915249f4b7e
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-12 20:26:19 -0400

    cases

--- chemistry.js
+++ chemistry.js
@@ -25,3 +25,28 @@ function solveChemistry(input) {
 
   const mode = match[1].toLowerCase();
   const args = splitChemArgs(match[2]);
+
+  const mode = match[1].toLowerCase();
+  const args = splitChemArgs(match[2]);
+
+  switch (mode) {
+    case 'stoichiometry': return solveStoichiometry(args);
+    case 'molarity': return solveMolarity(args);
+    case 'molality': return solveMolality(args);
+    case 'idealgas': return solveIdealGas(args);
+    case 'vanderwaals': return solveVanDerWaals(args);
+    case 'enthalpy': return solveEnthalpy(args);
+    case 'entropy': return solveEntropy(args);
+    case 'gibbs': return solveGibbs(args);
+    case 'equilibrium': return solveEquilibrium(args);
+    case 'ratelaw': return solveRateLaw(args);
+    case 'arrhenius': return solveArrhenius(args);
+    case 'nernst': return solveNernst(args);
+    case 'electrolysis': return solveElectrolysis(args);
+    case 'ph': return solvePH(args);
+    case 'buffer': return solveBuffer(args);
+    case 'quantumenergy': return solveQuantumEnergy(args);
+    default:
+      throw new Error("Unknown chemistry mode.");
+  }
+}
