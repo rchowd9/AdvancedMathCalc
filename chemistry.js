44b4436ce8ecf1ef24adad7220345b08ffd6ba3e
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-12 20:25:39 -0400

    added chem.js

---
+++ chemistry.js
@@ -0,0 +1,18 @@
+window.CHEMISTRY_MODE_NAMES = new Set([
+  'stoichiometry',
+  'molarity',
+  'molality',
+  'idealGas',
+  'vanDerWaals',
+  'enthalpy',
+  'entropy',
+  'gibbs',
+  'equilibrium',
+  'rateLaw',
+  'arrhenius',
+  'nernst',
+  'electrolysis',
+  'ph',
+  'buffer',
+  'quantumEnergy'
+]);
