796ba00ca4d5ad7efd4af11362a2445de21502e6
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-23 18:10:23 -0400

    added mechanical file

---
+++ mechanical.js
@@ -0,0 +1,25 @@
+// mechanical.js
+// Mechanical Engineering Module for Engineering Quest
+ 
+window.MECHANICAL_FORMULAS = new Set([
+'stress',
+'strain',
+'youngsmodulus',
+'shearstress',
+'torsion',
+'beamdeflection',
+'springforce',
+'buckling',
+'kineticenergy',
+'potentialenergy',
+'angularmomentum',
+'flywheelenergy',
+'gearratio',
+'shafttorque',
+'brakepower',
+'thermalexpansion',
+'thermalstress',
+'vibrationfrequency',
+'fluidpower',
+'bearinglife'
+]);
