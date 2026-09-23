f3ce1739d00b5ed744e43580e1981c615010eb95
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-23 18:15:01 -0400

    added electrical

---
+++ electrical.js
@@ -0,0 +1,25 @@
+// electrical.js
+// Electrical Engineering Module
+ 
+window.ELECTRICAL_FORMULAS = new Set([
+'ohmslaw',
+'electricpower',
+'resistance',
+'conductance',
+'capacitance',
+'inductance',
+'reactance',
+'impedance',
+'kirchhoffcurrent',
+'kirchhoffvoltage',
+'rccharge',
+'rldecay',
+'resonance',
+'transformer',
+'threephasepower',
+'powerfactor',
+'apparentpower',
+'reactivepower',
+'electricfield',
+'magneticflux'
+]);
